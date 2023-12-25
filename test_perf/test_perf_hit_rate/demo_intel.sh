22b21dfc907691ada0a1966826b9d50de2121bc3
jin11109 <wuchejin@gmail.com>
2023-12-25 10:47:24 +0800

    test perf hit rate 12/25

--- test_perf/test_perf_hit_rate/demo_intel.sh
+++ test_perf/test_perf_hit_rate/demo_intel.sh
@@ -7,9 +7,9 @@ echo 200000 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate > /dev/null
 # 39750
 
 # compile program
-gcc -O0 ./test_perf.c -o ./test_perf -lm
+gcc -O0 ./test_perf_intel.c -o ./test_perf_intel -lm
 
-./test_perf
+./test_perf_intel
 
 perf script -F +addr,+time,+data_src --ns -i ./perf.data > ./script.log
 
