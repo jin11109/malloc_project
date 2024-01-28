d6b13226a67d99ba46542a27cb62fa3cb6695b61
jin11109 <wuchejin@gmail.com>
2024-01-28 10:48:20 +0800

    test perf count 20240128

--- test_perf/test_perf_hit_random/demo_intel.sh
+++ test_perf/test_perf_hit_random/demo_intel.sh
@@ -13,7 +13,8 @@ gcc -O3 -g ./test_perf_hit_random.c -o ./test_perf_hit_random
 for (( i=0; i<5; i=i+1 )); do
     echo 150000 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate > /dev/null
 
-    perf record -e mem-stores:ppp,mem-loads:ppp -F max --running-time --data ./test_perf_hit_random > ./temp.log
+    #perf record -e mem-stores:ppp,mem-loads:ppp -F max --running-time --data ./test_perf_hit_random > ./temp.log
+    perf record -e mem-stores:ppp,mem-loads:ppp --count=6000 --running-time --data ./test_perf_hit_random > ./temp.log    
     perf script -F +addr,+time,+data_src --ns -i ./perf.data > ./script.log
     
     python3 ./filter.py
