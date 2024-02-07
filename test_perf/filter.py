f7b652a84e1282c099399e1ea567bc84929f835a
jin11109 <wuchejin@gmail.com>
2024-02-07 15:36:44 +0800

    update new test perf

--- test_perf/filter.py
+++ test_perf/filter.py
@@ -7,7 +7,7 @@ with open("./temp.log", 'r') as f:
     for i in range(0, 4):
         data = f.readline()
         number = re.findall(r'0x[0-9a-fA-F]+|\d+', data)
-        bin_addr.append((int(number[1], 16), int(number[0], 16)))
+        bin_addr.append((int(number[1], 16), int(number[2], 16)))
         print(f"bin{i} addr :", bin_addr[-1])
 
     data = f.readline()
