cca601660e705835fa0f1a7043bd4a3f7403259c
jin11109 <wuchejin@gmail.com>
2024-03-11 14:21:20 +0800

    update test perf 20240311

--- test_perf/demo.sh
+++ test_perf/demo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "demo.sh : parameter error (usage : demo.sh counts rounds)"
+    exit 0
+fi
+echo "coubt $1, rounds $2"
+
 # set enviroment
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid > /dev/null
 echo 0 | sudo tee /proc/sys/kernel/kptr_restrict > /dev/null
@@ -10,11 +16,11 @@ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
 gcc -O3 -g ./test_perf.c -o ./test_perf
 
 #perf stat -e instructions ./test_perf_hit_random
-for (( i=0; i<5; i=i+1 )); do
+for (( i=0; i<$2; i=i+1 )); do
     echo 150000 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate > /dev/null
 
     #perf record -e ibs_op/cnt_ctl=1/pp -F max --running-time --data ./test_perf > ./temp.log
-    perf record -e ibs_op/cnt_ctl=1/pp --count=50000 --timestamp --data ./test_perf > ./temp.log
+    perf record -e ibs_op/l3missonly=1,cnt_ctl=1/pp --count=$1 --timestamp --data ./test_perf > ./temp.log
     perf script -F +addr,+time,+data_src --ns -i ./perf.data > ./script.log
     
     python3 ./filter.py
--- test_perf/test_perf_hit_random/demo_intel.sh
+++ test_perf/test_perf_hit_random/demo_intel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "demo.sh : parameter error (usage : demo.sh counts rounds)"
+    exit 0
+fi
+echo "coubt $1, rounds $2"
 # set enviroment
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid > /dev/null
 echo 0 | sudo tee /proc/sys/kernel/kptr_restrict > /dev/null
@@ -10,11 +15,11 @@ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
 gcc -O3 -g ./test_perf_hit_random.c -o ./test_perf_hit_random
 
 #perf stat -e instructions ./test_perf_hit_random
-for (( i=0; i<1; i=i+1 )); do
+for (( i=0; i<$2; i=i+1 )); do
     echo 150000 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate > /dev/null
 
     #perf record -e mem-stores:ppp,mem-loads:ppp -F max --running-time --data ./test_perf_hit_random > ./temp.log
-    perf record -e MEM_UOPS_RETIRED.ALL_STORES:ppp,MEM_UOPS_RETIRED.ALL_LOADS:ppp --count=20000 --timestamp --data ./test_perf_hit_random > ./temp.log
+    perf record -e MEM_UOPS_RETIRED.ALL_STORES:ppp,MEM_UOPS_RETIRED.ALL_LOADS:ppp --count=$1 --timestamp --data ./test_perf_hit_random > ./temp.log
     perf script -F +addr,+time,+data_src --ns -i ./perf.data > ./script.log
     
     python3 ./filter.py
