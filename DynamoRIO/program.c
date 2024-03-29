5be21400eb3beda7cce01c212dde8cbf05821a83
jin11109 <wuchejin@gmail.com>
2024-03-29 17:04:23 +0800

    Fix the error of excuting the shell script

--- DynamoRIO/program.c
+++ DynamoRIO/program.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 int main(int argc, char *argv[]) {
 
-    setenv("LD_PRELOAD", "/lib/mymalloc.so", 1);
+    int fifo_fd = open("./fifo_preload", O_WRONLY);
+    if (fifo_fd == -1) {
+        perror("open");
+        return 1;
+    }
 
+    if (dup2(fifo_fd, 2) == -1) {
+        perror("dup2");
+        return 1;
+    }
+
+    setenv("LD_PRELOAD", "/lib/mymalloc.so", 1);
+    
     if (execvp(argv[1], &argv[1]) == -1) {
         perror("execvp");
         return 1;
     }
+
+    close(fifo_fd);
 
     return 0;
 }
