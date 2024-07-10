89c9d598d3f8cc147649372dd3077f53bf2c4976
jin11109 <wuchejin@gmail.com>
2024-07-10 15:03:58 +0800

    Update to replace the way of output as fprintf

--- two_mallocs/mymalloc_with_cold.c
+++ two_mallocs/mymalloc_with_cold.c
@@ -22,137 +22,6 @@
 #include <dlfcn.h>
 #include "ptmalloc2_with_cold/_my_malloc.h"
 
-/*============================ tools =======================================
- *==========================================================================*/
-
-int addr_to_string(void* addr, char* buffer) {
-    unsigned long a = (unsigned long)addr;
-    char num[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-    int size = 2;
-    while (a > 0) {
-        buffer[size] = num[a % 16];
-        a /= 16;
-        size++;
-    }
-
-    for (int i = 0; i < (size - 2) / 2; i++) {
-        char temp;
-        temp = buffer[i + 2];
-        buffer[i + 2] = buffer[size - i - 1];
-        buffer[size - i - 1] = temp;
-    }
-    buffer[0] = '0';
-    buffer[1] = 'x';
-    // buffer[size] = '\n';
-    buffer[size] = '\0';
-
-    return size;
-}
-
-int num_to_string(unsigned long long num, char* buffer) {
-    if (num == 0) {
-        buffer[0] = '0';
-        buffer[1] = '\0';
-        return 1;
-    }
-
-    int digit = 0;
-    long long int temp = num;
-    while (temp > 0) {
-        temp /= 10;
-        digit++;
-    }
-
-    int index = digit;
-    // char string[digit + 1];
-
-    while (num > 0) {
-        buffer[index - 1] = num % 10 + '0';
-        num /= 10;
-        index--;
-    }
-    buffer[digit] = '\0';
-
-    return digit;
-}
-
-void print_info_free(pid_t pid, void* addr) {
-    char addr_s[30], pid_s[10], time_s[30], buffer[110];
-    int addrlen, pidlen, timelen;
-    addrlen = addr_to_string(addr, addr_s);
-    pidlen = num_to_string(pid, pid_s);
-
-    int index = 0;
-    buffer[index++] = 'm';
-    buffer[index++] = 'y';
-    buffer[index++] = 'f';
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < pidlen; i++) {
-        buffer[index + i] = pid_s[i];
-    }
-    index += pidlen;
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < addrlen; i++) {
-        buffer[index + i] = addr_s[i];
-    }
-    index += addrlen;
-
-    buffer[index++] = '\n';
-
-    int wsize = write(2, buffer, index);
-}
-
-void print_info_alloc(pid_t pid, size_t size, void* addr, void* malloc_addr, char alloc_type) {
-    char addr_s[30], pid_s[10], size_s[30], malloc_addr_s[30], time_s[30],
-        buffer[140];
-    int addrlen, pidlen, sizelen, maddrlen, timelen;
-    addrlen = addr_to_string(addr, addr_s);
-    pidlen = num_to_string(pid, pid_s);
-    sizelen = num_to_string(size, size_s);
-    maddrlen = addr_to_string(malloc_addr, malloc_addr_s);
-
-    int index = 0;
-    buffer[index++] = 'm';
-    buffer[index++] = 'y';
-    buffer[index++] = 'a';
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < pidlen; i++) {
-        buffer[index + i] = pid_s[i];
-    }
-    index += pidlen;
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < sizelen; i++) {
-        buffer[index + i] = size_s[i];
-    }
-    index += sizelen;
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < addrlen; i++) {
-        buffer[index + i] = addr_s[i];
-    }
-    index += addrlen;
-    buffer[index++] = ' ';
-
-    for (int i = 0; i < maddrlen; i++) {
-        buffer[index + i] = malloc_addr_s[i];
-    }
-    index += maddrlen;
-    buffer[index++] = ' ';
-
-    // record is malloc or realloc or calloc
-    buffer[index++] = alloc_type;
-
-    buffer[index++] = '\n';
-
-    int wsize = write(2, buffer, index);
-}
-
 /*============================ prelaod functions ============================
  *==========================================================================*/
 
@@ -161,7 +30,7 @@ void* malloc(size_t bytes) {
     void* return_addr = __builtin_extract_return_addr(__builtin_return_address(
         0)); // value of 0 yields return address of the current function
     if (bytes != 0)
-        print_info_alloc(getpid(), bytes, ptr, return_addr, 'm');
+        fprintf(stderr, "mya %d %ld %p %p %c\n", getpid(), bytes, ptr, return_addr, 'm');
 
     return ptr;
 }
@@ -171,7 +40,7 @@ void* realloc(void* addr, size_t size) {
     void* return_addr = __builtin_extract_return_addr(__builtin_return_address(
         0)); // value of 0 yields return address of the current function
     if (size != 0)
-        print_info_alloc(getpid(), size, ptr, return_addr, 'r');
+        fprintf(stderr, "mya %d %ld %p %p %c\n", getpid(), size, ptr, return_addr, 'r');
     
     return ptr;
 }
@@ -180,7 +49,8 @@ void* calloc(size_t nmemb, size_t size) {
     void* ptr = _my_calloc(nmemb, size);
     void* return_addr = __builtin_extract_return_addr(__builtin_return_address(
         0)); // value of 0 yields return address of the current function
-    print_info_alloc(getpid(), size, ptr, return_addr, 'c');
+    if (nmemb * size != 0)
+        fprintf(stderr, "mya %d %ld %p %p %c\n", getpid(), size, ptr, return_addr, 'c');
     return ptr;
 }
 
