ffb687a15564bc6a2ae23577c66deb7b4f5f027c
jin11109 <wuchejin@gmail.com>
2024-08-28 22:08:33 +0800

    Update the way of outputting  cold_alloc_addr_table.csv

--- tools/print_malloc_addr.py
+++ tools/print_malloc_addr.py
@@ -1,15 +1,35 @@
 import os
 import re
+import csv
 
-path = input("path:\n")
+path = input("dir path:\n")
+pid = input("pid:\n")
+temperature = input("temperature:\n")
+
 all_data = os.listdir(path)
 result = []
 for data in all_data:
     if re.search(r'all_obj', data) is not None:
         output = data.split("_")
         result.append((int(output[0]), output[1]))
-        
+
+filename = 'cold_alloc_addr_table.csv'
+file_exists = os.path.exists(filename)
+if file_exists:
+    data = []
+else:
+    data = [
+        ['pid_in_drcachesim', 'pid_real', 'alloc_index_in_drcachesim', 'alloc_addr_in_drcachesim', 
+         'alloc_addr_real', 'alloc_index_real', 'temperature', 'symbol', 'position', 'mark'],
+    ]
+
 result.sort()
-print("pid_in_drcachesim,pid_real,alloc_index_in_drcachesim,alloc_addr_in_drcachesim,alloc_addr_real,alloc_index_real,temperature,mark")
-for i in result:        
-    print(f'{i[0]},{i[1]},')
+for i in result:
+    per_data = [pid, None, i[0], i[1], None, None, temperature, None, None, None]
+    data.append(per_data)
+
+
+with open(filename, 'a' if file_exists else 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for row in data:
+        csvwriter.writerow(row)
