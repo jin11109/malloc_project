50dac3ef402cbc3df3f58dbc5c340b7c567e4436
jin11109 <wuchejin@gmail.com>
2024-08-30 00:05:42 +0800

    Add both test of different calculating ways of offset

--- tools/test_addr_conflict.py
+++ tools/test_addr_conflict.py
@@ -2,46 +2,53 @@ import os
 import re
 import csv
 
+PAGE_SIZE = 4096
 TABLE_SIZE = 4096 * 16
-cold_dir = []
-all_dir = []
-cold_addr = {}
-other_addr = {}
-cold_offset = {}
-other_offset = {}
 
-path = input("input cold dir path:\n")
-while len(path) != 0:
-    cold_dir.append(path)
-    path = input("input cold dir path, if done just press enter:\n")
-path = input("input all dir path:\n")
-while len(path) != 0:
-    all_dir.append(path)
-    path = input("input all dir path, if done just press enter:\n")
+def test(size):
+    cold_addr = {}
+    other_addr = {}
+    cold_offset = {}
+    other_offset = {}
 
-for path in cold_dir:
-    all_data = os.listdir(path)
-    for data in all_data:
-        if re.search(r'all_obj', data) is not None:
-            output = data.split("_")
-            cold_addr[output[1]] = True
-for path in all_dir:
-    all_data = os.listdir(path)
-    for data in all_data:
-        if re.search(r'all_obj', data) is not None:
-            output = data.split("_")
-            if cold_addr.get(output[1]) is None:
-                other_addr[output[1]] = True
+    for path in cold_dir:
+        all_data = os.listdir(path)
+        for data in all_data:
+            if re.search(r'all_obj', data) is not None:
+                output = data.split("_")
+                cold_addr[output[1]] = True
+    for path in all_dir:
+        all_data = os.listdir(path)
+        for data in all_data:
+            if re.search(r'all_obj', data) is not None:
+                output = data.split("_")
+                if cold_addr.get(output[1]) is None:
+                    other_addr[output[1]] = True
 
-for addr in cold_addr:
-    offset = int(addr, 16) & (TABLE_SIZE - 1)
-    cold_offset[offset] = True
-errorflag = False
-for addr in other_addr:
-    offset = int(addr, 16) & (TABLE_SIZE - 1)
-    if cold_offset.get(offset):
-        errorflag = True
-        print("other addr :", addr, "\noffset :", offset)
+    for addr in cold_addr:
+        offset = int(addr, 16) & (size - 1)
+        cold_offset[offset] = True
+    errorflag = False
+    for addr in other_addr:
+        offset = int(addr, 16) & (size - 1)
+        if cold_offset.get(offset):
+            errorflag = True
+            print("other addr :", addr, "\noffset :", offset)
 
-if errorflag == False:
-    print("ok")
+    if errorflag == False:
+        print("ok")
+
+if __name__ == "__main__":
+    cold_dir = []
+    all_dir = []
+    path = input("input cold dir path:\n")
+    while len(path) != 0:
+        cold_dir.append(path)
+        path = input("input cold dir path, if done just press enter:\n")
+    path = input("input all dir path:\n")
+    while len(path) != 0:
+        all_dir.append(path)
+        path = input("input all dir path, if done just press enter:\n")
+
+    test(PAGE_SIZE)
+    test(TABLE_SIZE)
