f6cc2246885533c3b4716780bb3bedd61a97af6c
jin11109 <wuchejin@gmail.com>
2024-10-16 06:18:11 +0800

    Add a new sell script to simulate using program

---
+++ drcachesim/program_simulation/netsurf.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+end_time=$((1200))
+
+while [ $SECONDS -lt $end_time ]; do
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 5
+    xdotool click 5
+    sleep 1
+    xdotool click 4
+    xdotool click 4
+    sleep 1
+    xdotool click 4
+    xdotool click 4
+    sleep 1
+    xdotool click 4
+    xdotool click 4
+    sleep 1
+    xdotool click 4
+    xdotool click 4
+    sleep 1
+    xdotool click 4
+    xdotool click 4
+
+    sleep $((RANDOM % 3 + 1))
+done
